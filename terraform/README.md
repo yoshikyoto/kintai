714fe5ae5bbb6d2012ee9ca1065694facbad2714
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2023-04-08 09:17:07 +0900

    wip

--- terraform/README.md
+++ terraform/README.md
@@ -1,6 +1,19 @@
 
 ## CDK for Terraform のインストール
 
+tfenv を使って terraform をインストールする
+
+```bash
+# Mac の場合
+$ brew install tfenv
+
+# TODO terraform のバージョンをなんかいい感じにインストールするやつ
+$ tfenv install 1.4.4
+$ tfenv use 1.4.4
+```
+
+CDK for 
+
 ```bash
 npm install --global cdktf-cli
 ```
--- terraform/main.ts
+++ terraform/main.ts
@@ -1,14 +1,25 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
+import { AppEngineApplication } from "@cdktf/provider-google/lib/app-engine-application";
+import { GoogleProvider } from "@cdktf/provider-google/lib/provider";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
+    const projectId = "kintai-378808";
+    const locationId = "asia-northeast1";
 
-    // define resources here
+    new GoogleProvider(this, "google", {
+      project: projectId,
+    });
+
+    new AppEngineApplication(this, "kintai-appengine", {
+      project: projectId,
+      locationId: locationId,
+    });
   }
 }
 
 const app = new App();
-new MyStack(app, "terraform");
+new MyStack(app, "kintai-terraform");
 app.synth();
