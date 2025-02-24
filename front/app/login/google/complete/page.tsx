49ba59fc072495132d376931b1eab0b50de5955a
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2025-02-24 18:08:05 +0900

    google に遷移するまで

---
+++ front/app/login/google/complete/page.tsx
@@ -0,0 +1,8 @@
+export default function GoogleLoginComplete() {
+  return (
+    <>
+      <h1>Google ログイン完了</h1>
+      <p>Google ログインが完了しました。このページからバックエンドに情報を送信します。</p>
+    </>
+  );
+}
--- front/app/login/page.tsx
+++ front/app/login/page.tsx
@@ -1,7 +1,14 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
-      <>
-        ログイン
-      </>
+    <>
+      <h1>ログイン</h1>
+      <a href={`https://accounts.google.com/o/oauth2/v2/auth?client_id=YOUR_DUMMY_CLIENT_ID&redirect_uri=${encodeURIComponent(
+        '/login/google/complete'
+      )}&response_type=code&scope=openid%20profile%20email`}>
+        Googleでログイン
+      </a>
+    </>
   );
 }
