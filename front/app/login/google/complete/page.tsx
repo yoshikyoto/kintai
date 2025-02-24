2b406674dd511ac1dc798c7c099fc2e1bed37248
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2025-02-24 22:28:43 +0900

    cors 設定追加

--- front/app/login/google/complete/page.tsx
+++ front/app/login/google/complete/page.tsx
@@ -40,7 +40,7 @@ export default function GoogleLoginComplete() {
   return (
     <>
       <h1>Google ログイン完了</h1>
-      <p>Google ログインが完了しました。このページからバックエンドに情報を送信します。</p>
+      <p>しばらくお待ち下さい</p>
     </>
   );
 }
