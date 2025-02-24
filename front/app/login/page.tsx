c60d7965888a712c157ac80d62711be684be4ba8
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2025-02-24 21:52:02 +0900

    login 周りを修正

--- front/app/login/page.tsx
+++ front/app/login/page.tsx
@@ -1,11 +1,14 @@
 import Link from 'next/link';
 
 export default function Home() {
+  const googleApiKey = "594257163006-vjl4qre2a74uokpv290nuk9smqh3ium5.apps.googleusercontent.com";
+  const redirectUrl = "http://localhost:3000/login/google/complete";
+
   return (
     <>
       <h1>ログイン</h1>
-      <a href={`https://accounts.google.com/o/oauth2/v2/auth?client_id=YOUR_DUMMY_CLIENT_ID&redirect_uri=${encodeURIComponent(
-        '/login/google/complete'
+      <a href={`https://accounts.google.com/o/oauth2/v2/auth?client_id=${googleApiKey}&redirect_uri=${encodeURIComponent(
+        'redirectUrl'
       )}&response_type=code&scope=openid%20profile%20email`}>
         Googleでログイン
       </a>
